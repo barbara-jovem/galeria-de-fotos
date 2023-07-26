ac0d592d58c25a38a5e24d28bfc8aed244ac1152
Bárbara Jovem <128049186+barbara-jovem@users.noreply.github.com>
2023-07-25 21:41:22 -0300

    Finalização do Exercício

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-$(document).ready(function() {
-    $('header button').click(function() {
-        $('form').slideDown();
-    })
+$(document).ready(function () {
+  $("header button").click(function () {
+    $("form").slideDown();
+  });
 
-    $('#botao-cancelar').click(function() {
-        $('form').slideUp();
-    })
+  $("#botao-cancelar").click(function () {
+    $('form').slideUp();
+  });
 
-    $('form').on('submit' , function(e) {
-        e.preventDefault();
-        const endereçoNovaImagem = $('endereço-imagem-nova').val();
-        const novoItem = $('<li></li>');
-        $(`<img src="${endereçoNovaImagem}" />`).appendTo(novoItem);
-        $(`
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    const endereçoNovaImagem = $('#endereço-imagem-nova').val();
+    const novoItem = $('<li style="display: none" ></li>');
+    $(`<img src="${endereçoNovaImagem}" />`).appendTo(novoItem);
+    $(`
         <div class="overlay-imagem-link">
             <a href="${endereçoNovaImagem}" target ="_blank" title="Ver imagem em tamanho real">
                 Ver imagem em tamanho real
             </a>
         </div>
         `).appendTo(novoItem);
-        $(novoItem).appendTo('ul');
-    })
-})
+    $(novoItem).appendTo("ul");
+    $(novoItem).fadeIn(1000);
+    $('#endereço-imagem-nova').val('');
+  });
+});
 //quando o documento estiver pronto, ele deve executar o alert
